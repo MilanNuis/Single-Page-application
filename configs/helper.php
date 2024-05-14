edc0983f043a7302582ce17cee9ea8eb9ce14dde
Milan Nuis <57843223+MilanNuis@users.noreply.github.com>
2024-05-14 14:53:39 +0200

    Update helper.php

--- configs/helper.php
+++ configs/helper.php
@@ -4,7 +4,6 @@ function getPage()
 {
     if (isset($_GET['page'])) {
         if (file_exists("resources/views/" . $_GET['page'] . ".view.php")) {
-            echo "Gevonden";
             return "resources/views/" . $_GET['page'] . ".view.php";
         } else {
             return "resources/views/errors/404.view.php";
