48c71599c48ad794d9773f8c8e67a7feddcddfa0
Jaron De Klein <153074306+CodebyJaron@users.noreply.github.com>
2024-05-16 14:12:48 +0200

    done

--- components/nav.php
+++ components/nav.php
@@ -18,17 +18,18 @@
             <a href="?page=media" class="text-sm font-semibold leading-6 text-gray-900 border-b-2 px-2 transition duration-200 ease-in-out <?php echo $_GET['page'] == 'media' ? 'border-red-500' : 'border-transparent hover:border-red-500'; ?>">Media</a>
             <a href="?page=aboutUs" class="text-sm font-semibold leading-6 text-gray-900 border-b-2 px-2 transition duration-200 ease-in-out <?php echo $_GET['page'] == 'aboutUs' ? 'border-red-500' : 'border-transparent hover:border-red-500'; ?>">Over ons</a>
         </div>
-        <div class="hidden lg:flex lg:flex-1 lg:justify-end space-x-4">
-            <div class="relative">
-                <div class="t-0 absolute left-3">
+        <div class="hidden lg:flex lg:flex-1 lg:justify-end space-x-6 items-center">
+            <a href="?page=cart" class="relative flex items-center">
+                <div class="absolute -top-2 left-3">
                     <p class="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">3</p>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="file: mt-4 h-6 w-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                 </svg>
-            </div>
+            </a>
             <a href="?page=login" class="text-sm font-semibold leading-6 hover:scale-105 transition ease-in-out duration-200 hover:text-gray-900/80 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
         </div>
+
     </nav>
     <!-- Mobile menu, show/hide based on menu open state. -->
     <div class="hidden" role="dialog" aria-modal="true">
