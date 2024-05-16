648df0db97f85558d1b92dd20d287d905e1cf5cd
Djurre Wiebenga <dwwiebenga@st.noorderpoort.nl>
2024-05-16 10:59:34 +0200

    footer/home

--- components/footer.php
+++ components/footer.php
@@ -2,7 +2,7 @@
 
 require_once("./unitils/connection.php");
 ?>
-<footer class="bg-gradient-to-r from-red-500 to-red-700 rounded-t-2xl" aria-labelledby="footer-heading">
+<footer class="overflow-hidden bg-gradient-to-r from-red-500 to-red-700 rounded-t-2xl" aria-labelledby="footer-heading">
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-16">
         <div class="xl:grid xl:grid-cols-3 xl:gap-8">
@@ -71,7 +71,7 @@ require_once("./unitils/connection.php");
                     <label for="email-address" class="sr-only">Email address</label>
                     <input type="email" name="email-address" id="email-address" autocomplete="email" required class="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-slate-800 shadow-sm ring-1 ring-inset ring-white-300 placeholder:text-white-400 focus:ring-2 focus:ring-inset focus:ring-indigo sm:w-64 sm:text-sm sm:leading-6 xl:w-full" placeholder="Enter your email">
                     <div class="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-                        <button type="submit" class="flex w-full items-center justify-center rounded-md bg-indigo px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo">Aboneer</button>
+                        <button type="submit" class="flex w-full items-center justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo">Abonneer</button>
                     </div>
                 </form>
                 <div class="flex flex-row">
@@ -116,7 +116,7 @@ require_once("./unitils/connection.php");
                     </svg>
                 </a>
             </div>
-            <p class="mt-8 text-xs leading-5 text-white md:order-1 md:mt-0">&copy; 2020 PMOT, All rights reserved.</p>
+            <p class="mt-8 text-xs leading-5 text-white md:order-1 md:mt-0">&copy; 2024 PMOT, All rights reserved.</p>
         </div>
     </div>
 </footer>
