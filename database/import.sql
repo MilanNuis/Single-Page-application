23c710e3e24e3173501c878e4d5354f027a274ab
Milan Nuis <57843223+MilanNuis@users.noreply.github.com>
2024-05-16 10:55:55 +0200

    Update import.sql

--- database/import.sql
+++ database/import.sql
@@ -2,16 +2,11 @@ DROP DATABASE IF EXISTS deepdive;
 CREATE DATABASE IF NOT EXISTS deepdive;
 USE deepdive;
 
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `name` VARCHAR(255)
-);
-
 CREATE TABLE IF NOT EXISTS `customers` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `firstname` VARCHAR(255),
   `lastname` VARCHAR(255),
-  `email` VARCHAR(255),
+  `email` VARCHAR(255) UNIQUE,
   `password` VARCHAR(255),
   `country` VARCHAR(255),
   `isAdmin` BOOLEAN DEFAULT false,
@@ -21,18 +16,17 @@ CREATE TABLE IF NOT EXISTS `customers` (
 CREATE TABLE IF NOT EXISTS `products` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(255),
+  `description` TEXT,
   `price` DECIMAL(10, 2),
   `stock` INT,
   `imageURL` varchar(255),
-  `category` INT,
-  CONSTRAINT `fk_product_category` FOREIGN KEY (`category`) REFERENCES `categories` (`id`)
+  `category` varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS `payments` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
   `ownerId` INT,
-  `paymentId` VARCHAR(255),
-  FOREIGN KEY (`ownerId`) REFERENCES `customers` (`id`)
+  `paymentId` VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS `addresses` (
@@ -50,10 +44,7 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `orderDate` DATE,
   `totalPrice` DECIMAL(10, 2),
   `addressInformation` INT,
-  `paymentInformation` INT,
-  FOREIGN KEY (`orderOwner`) REFERENCES `customers` (`id`),
-  FOREIGN KEY (`addressInformation`) REFERENCES `addresses` (`id`),
-  FOREIGN KEY (`paymentInformation`) REFERENCES `payments` (`id`)
+  `paymentInformation` INT
 );
 
 CREATE TABLE IF NOT EXISTS `newsletterEmail` (
